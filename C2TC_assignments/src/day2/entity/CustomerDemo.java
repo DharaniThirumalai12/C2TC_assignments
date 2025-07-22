7f84faf8c0483caa5e11dc3965ac0a280e405ed5
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-22 16:39:25 +0530

    Customer

---
+++ C2TC_assignments/src/day2/entity/CustomerDemo.java
@@ -0,0 +1,20 @@
+package day2.entity;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.setCid(101);
+		c1.setName("Dharani");
+		c1.setCity("Pondicherry");
+		System.out.println(c1);
+		Customer c2=new Customer();
+		c2.setCid(102);
+		c2.setName("Thirumalai");
+		c2.setCity("Pondicherry");
+		System.out.println(c2);
+	System.out.println("The Id of Customer 1 is: "+ c1.getCid());
+	System.out.println("The Name of Customer 2 is: "+ c2.getName());
+	System.out.println("The City of Customer 2 is: "+ c2.getCity());
+	}
+}
